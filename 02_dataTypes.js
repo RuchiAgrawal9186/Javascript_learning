a59c78c4db6ad90572eda5f7e0da46ac8e73ebb6
RuchiAgrawal01 <ruchi.agrawal@portqii.com>
2025-02-08 17:06:28 +0530

    data types learning

---
+++ 02_dataTypes.js
@@ -0,0 +1,17 @@
+// 'use strict' this is for to follow new syntax according new js strictly follow
+
+// primitive datatypes
+/* 1.number
+   2. string 
+   3 boolean
+   4 symbol
+   5 null -> soe value you want intencially empty
+   6 undefined -> means variable is declared but that value is not defined
+   */
+
+console.log(typeof null) // typeof function is helpful for check data type so for null datatype is object
+console.log(typeof undefined)   // for undefined type is undefined
+
+// non-primitive data types
+
+// 1. object
