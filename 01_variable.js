a39a18d1808d6f76f579c4359a6be815a38bbe94
RuchiAgrawal01 <ruchi.agrawal@portqii.com>
2025-02-08 16:40:02 +0530

    types of variable learning

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+let name="abc"
+const id = '12345'
+var city='dahod'
+
+console.log(name) // its used to print value in screen
+console.table([name,id,city])// its also print output in screen but in table formate
+
+/*using three types of keyword we can declare variable in js  
+1. const we can not change value after we declare once
+2. let we can change after declaration  also,mostly used keyword
+3. var is used in older javascript of new js we are not prefer to use var keyword because of block and function scope issue 
+*/
